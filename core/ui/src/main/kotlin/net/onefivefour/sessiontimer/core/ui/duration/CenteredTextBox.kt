dd15e85ab56a59c04c65c970241085d0ba67679c
Florian Meyer <florian.meyer@onefivefour.net>
2025-01-28 17:20:31 +0100

    preview centeredtextbox

--- core/ui/src/main/kotlin/net/onefivefour/sessiontimer/core/ui/duration/CenteredTextBox.kt
+++ core/ui/src/main/kotlin/net/onefivefour/sessiontimer/core/ui/duration/CenteredTextBox.kt
@@ -1,16 +1,23 @@
 package net.onefivefour.sessiontimer.core.ui.duration
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import net.onefivefour.sessiontimer.core.theme.SessionTimerTheme
 
 @Composable
 internal fun CenteredTextBox(
@@ -18,7 +25,7 @@ internal fun CenteredTextBox(
     content: @Composable () -> Unit,
 ) {
     val borderWidth = when {
-        isFocused -> 1.dp
+        isFocused -> 2.dp
         else -> 0.dp
     }
     Box(
@@ -26,13 +33,32 @@ internal fun CenteredTextBox(
         modifier = Modifier
             .size(TILE_SIZE)
             .clip(RoundedCornerShape(8.dp))
-            .background(MaterialTheme.colorScheme.onSurfaceVariant)
+            .background(MaterialTheme.colorScheme.surfaceVariant)
             .border(
                 width = borderWidth,
-                color = MaterialTheme.colorScheme.primary,
+                color = MaterialTheme.colorScheme.onSurface,
                 shape = RoundedCornerShape(8.dp)
             )
     ) {
         content()
+    }
+}
+
+@Preview
+@Preview(uiMode = UI_MODE_NIGHT_YES)
+@Composable
+private fun CenteredTextBoxPreview() {
+    SessionTimerTheme {
+        Surface {
+            CenteredTextBox(isFocused = true) {
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = "123",
+                    color = MaterialTheme.colorScheme.onSurface,
+                    style = MaterialTheme.typography.labelSmall,
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
     }
 }
--- core/ui/src/main/kotlin/net/onefivefour/sessiontimer/core/ui/duration/DurationInput.kt
+++ core/ui/src/main/kotlin/net/onefivefour/sessiontimer/core/ui/duration/DurationInput.kt
@@ -15,7 +15,10 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
@@ -52,7 +55,7 @@ fun DurationInput(
         backgroundColor = Color.Transparent,
     )
 
-    var isFocused = remember { false }
+    var isFocused by remember { mutableStateOf(false) }
 
     val focusRequester = remember { FocusRequester() }
 
