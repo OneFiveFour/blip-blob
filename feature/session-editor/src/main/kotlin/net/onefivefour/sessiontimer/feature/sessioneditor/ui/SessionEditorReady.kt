6bb4a6729a91cf3e9f0d1a4e3c437fdc7a0b034b
Florian Meyer <florian.meyer@onefivefour.net>
2025-02-01 13:08:00 +0100

    start with taskgroup paging

--- feature/session-editor/src/main/kotlin/net/onefivefour/sessiontimer/feature/sessioneditor/ui/SessionEditorReady.kt
+++ feature/session-editor/src/main/kotlin/net/onefivefour/sessiontimer/feature/sessioneditor/ui/SessionEditorReady.kt
@@ -1,42 +1,49 @@
 package net.onefivefour.sessiontimer.feature.sessioneditor.ui
 
-import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.text.input.TextFieldLineLimits
+import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.key
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
-import androidx.compose.ui.text.TextRange
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
+import kotlinx.coroutines.launch
 import net.onefivefour.sessiontimer.core.theme.SessionTimerTheme
 import net.onefivefour.sessiontimer.core.ui.R
-import net.onefivefour.sessiontimer.core.ui.sqarebutton.SquareButton
-import net.onefivefour.sessiontimer.core.ui.haptic.ReorderHapticFeedbackType
-import net.onefivefour.sessiontimer.core.ui.haptic.rememberReorderHapticFeedback
 import net.onefivefour.sessiontimer.core.ui.label.LabeledSection
 import net.onefivefour.sessiontimer.core.ui.modifier.clearFocusOnKeyboardDismiss
 import net.onefivefour.sessiontimer.core.ui.screentitle.ScreenTitle
+import net.onefivefour.sessiontimer.core.ui.sqarebutton.SquareButton
 import net.onefivefour.sessiontimer.core.ui.utils.topToAscentDp
 import net.onefivefour.sessiontimer.feature.sessioneditor.model.UiSession
 import net.onefivefour.sessiontimer.feature.sessioneditor.viewmodel.SessionEditorAction
-import sh.calvin.reorderable.ReorderableColumn
 
 @Composable
 internal fun SessionEditorReady(
@@ -44,13 +51,20 @@ internal fun SessionEditorReady(
     onAction: (SessionEditorAction) -> Unit,
     openTaskGroupEditor: (Long) -> Unit,
 ) {
-    val haptic = rememberReorderHapticFeedback()
 
-    var taskGroupList by remember(uiSession.taskGroups) { mutableStateOf(uiSession.taskGroups) }
+    val pagerState = rememberPagerState { uiSession.taskGroups.size }
 
-    var collapsedTaskGroupsIds by remember { mutableStateOf(setOf<Long>()) }
+    val coroutineScope = rememberCoroutineScope()
 
-    Column(modifier = Modifier.fillMaxWidth()) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(
+                start = 24.dp,
+                end = 24.dp,
+                bottom = 24.dp
+            )
+    ) {
 
         ScreenTitle(titleRes = R.string.edit_session)
 
@@ -58,81 +72,92 @@ internal fun SessionEditorReady(
             val textStyle = MaterialTheme.typography.titleMedium
             val offset = textStyle.topToAscentDp() - 4.dp
 
-            // TODO replace by new basictextfield
+            val textFieldState = rememberTextFieldState()
+
+            LaunchedEffect(uiSession.title) {
+                textFieldState.edit {
+                    replace(
+                        start = 0,
+                        end = length,
+                        text = uiSession.title
+                    )
+                }
+            }
             BasicTextField(
                 modifier = Modifier
                     .zIndex(1f)
                     .offset(y = offset)
                     .fillMaxWidth()
                     .clearFocusOnKeyboardDismiss(),
-                value = TextFieldValue(
-                    text = uiSession.title,
-                    selection = TextRange(uiSession.title.length)
-                ),
-                onValueChange = { newText -> onAction(SessionEditorAction.SetSessionTitle(newText.text)) },
+                inputTransformation = {
+                    val newTitle = asCharSequence().toString()
+                    onAction(SessionEditorAction.SetSessionTitle(newTitle))
+                },
+                state = textFieldState,
                 cursorBrush = SolidColor(MaterialTheme.colorScheme.onSurface),
-                singleLine = true,
+                lineLimits = TextFieldLineLimits.SingleLine,
                 textStyle = MaterialTheme.typography.titleMedium
                     .copy(color = MaterialTheme.colorScheme.onSurface),
             )
         }
 
-        ReorderableColumn(
-            modifier = Modifier
-                .weight(1f)
-                .verticalScroll(rememberScrollState())
-                .padding(8.dp),
-            list = taskGroupList,
-            onSettle = { fromIndex, toIndex ->
-                taskGroupList = taskGroupList.toMutableList().apply {
-                    add(toIndex, removeAt(fromIndex))
-                }
-                val taskGroupIds = taskGroupList.map { it.id }
-                onAction(SessionEditorAction.UpdateTaskGroupSortOrders(taskGroupIds))
-            },
-            onMove = {
-                haptic.performHapticFeedback(ReorderHapticFeedbackType.MOVE)
-            },
-            verticalArrangement = Arrangement.spacedBy(8.dp)
-        ) { _, taskGroup, _ ->
-            key(taskGroup.id) {
-                val interactionSource = remember { MutableInteractionSource() }
-
-                var isDragging by remember { mutableStateOf(false) }
-
-                TaskGroupItem(
-                    modifier = Modifier
-                        .longPressDraggableHandle(
-                            onDragStarted = {
-                                haptic.performHapticFeedback(ReorderHapticFeedbackType.START)
-                                isDragging = true
-                            },
-                            onDragStopped = {
-                                haptic.performHapticFeedback(ReorderHapticFeedbackType.END)
-                                isDragging = false
-                            },
-                            interactionSource = interactionSource
-                        ),
-                    uiTaskGroup = taskGroup,
-                    isCollapsed = collapsedTaskGroupsIds.contains(taskGroup.id),
-                    isDragging = isDragging,
-                    onCollapseChanged = { isCollapsed ->
-                        collapsedTaskGroupsIds = when {
-                            isCollapsed -> collapsedTaskGroupsIds + taskGroup.id
-                            else -> collapsedTaskGroupsIds - taskGroup.id
-                        }
-                    },
-                    openTaskGroupEditor = { openTaskGroupEditor(taskGroup.id) },
-                    onAction = onAction,
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier.weight(1f),
+        ) { page ->
+            Box(
+                modifier =
+                Modifier
+                    .padding(10.dp)
+                    .background(Color.Blue)
+                    .fillMaxWidth()
+                    .aspectRatio(1f),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    modifier = Modifier.clickable { openTaskGroupEditor(uiSession.taskGroups[page].id) },
+                    text = uiSession.taskGroups[page].title, fontSize = 32.sp
                 )
             }
         }
 
-        SquareButton(
-            iconRes = R.drawable.ic_add,
-            contentDescriptionRes = R.string.new_task_group,
-            onClick = { onAction(SessionEditorAction.CreateTaskGroup) }
-        )
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.End
+        ) {
+
+            LazyRow(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                items(uiSession.taskGroups.size) { index ->
+                    val isSelected = pagerState.currentPage == index
+                    val size = if (isSelected) 12.dp else 8.dp
+                    Box(
+                        modifier = Modifier
+                            .padding(4.dp)
+                            .size(size)
+                            .background(
+                                color = uiSession.taskGroups[index].color,
+                                shape = CircleShape
+                            )
+                            .clickable {
+                                coroutineScope.launch {
+                                    pagerState.scrollToPage(index)
+                                }
+                            }
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.size(16.dp))
+
+            SquareButton(
+                iconRes = R.drawable.ic_add,
+                contentDescriptionRes = R.string.new_task_group,
+                onClick = { onAction(SessionEditorAction.CreateTaskGroup) }
+            )
+        }
     }
 }
 
@@ -145,6 +170,8 @@ private fun SessionEditorReadyPreview() {
                 uiSession = UiSession(
                     title = "Session Title",
                     taskGroups = listOf(
+                        fakeUiTaskGroup(),
+                        fakeUiTaskGroup(),
                         fakeUiTaskGroup()
                     )
                 ),
