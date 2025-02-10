46b5d69b4ef44201479c6fdc62df3e8bc6bf7453
Florian Meyer <florian.meyer@onefivefour.net>
2025-02-10 21:30:59 +0100

    include ime visibility for edit mode detection

--- app/src/main/kotlin/net/onefivefour/sessiontimer/MainActivity.kt
+++ app/src/main/kotlin/net/onefivefour/sessiontimer/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
 import dagger.hilt.android.AndroidEntryPoint
 import net.onefivefour.sessiontimer.core.theme.SessionTimerTheme
 import net.onefivefour.sessiontimer.core.ui.modifier.debugBorder
--- feature/session-editor/src/main/kotlin/net/onefivefour/sessiontimer/feature/sessioneditor/ui/SessionEditorReady.kt
+++ feature/session-editor/src/main/kotlin/net/onefivefour/sessiontimer/feature/sessioneditor/ui/SessionEditorReady.kt
@@ -5,10 +5,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -17,15 +19,21 @@ import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.State
 import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import net.onefivefour.sessiontimer.core.theme.SessionTimerTheme
 import net.onefivefour.sessiontimer.core.ui.R
+import net.onefivefour.sessiontimer.core.ui.keyboard.keyboardAsState
 import net.onefivefour.sessiontimer.core.ui.screentitle.ScreenTitle
 import net.onefivefour.sessiontimer.core.ui.sqarebutton.SquareButton
 import net.onefivefour.sessiontimer.feature.sessioneditor.model.UiSession
@@ -45,6 +53,14 @@ internal fun SessionEditorReady(
     val lazyListState = rememberLazyListState()
 
     val taskEditMode = remember { mutableStateOf<TaskEditMode>(TaskEditMode.None) }
+
+    val isImeVisible by keyboardAsState()
+
+    LaunchedEffect(isImeVisible) {
+        if (!isImeVisible) {
+            taskEditMode.value = TaskEditMode.None
+        }
+    }
 
     CompositionLocalProvider(LocalTaskEditMode provides taskEditMode) {
 
